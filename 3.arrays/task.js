fb241dc7e1d169de5b17753dcee01fabbf60253f
Alessio.Loginov <alekseyloginov79@gmail.com>
2023-07-04 06:09:08 +0300

    add tasks

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,20 @@
 function compareArrays(arr1, arr2) {
-  
+	let result = false;
+	if (arr1.length == arr2.length) {
+		result = arr1.every((element, index) => {
+			return element === arr2[index];
+		});
+	}
+	return result; // boolean
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let peoplesFilter = users.filter((user) => user.gender === gender);
+	let summAges = peoplesFilter
+		.map((user) => user.age)
+		.reduce((acc, userAge) => acc + userAge, 0);
+	if (isNaN(summAges / peoplesFilter.length)) {
+		return 0;
+	}
+	return summAges / peoplesFilter.length;
 }
